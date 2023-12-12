456c302f391b239a90220cd49a1a81db63e90698
EunChong999 <eunchonggim999@gmail.com>
2023-12-12 19:09:56 +0900

    feat : 회전 함수 수정

--- Assets/EunChong/Scripts/BasicClick.cs
+++ Assets/EunChong/Scripts/BasicClick.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BasicClick : MonoBehaviour
 {
@@ -10,34 +12,83 @@ public class BasicClick : MonoBehaviour
     [SerializeField] float targetRotation;
     [SerializeField] float rotationSpeed;
 
-    public void MaterialConvert()
-    {
-        if (gameObject.GetComponent<MeshRenderer>().sharedMaterial == fromMaterial)
-        {
-            Debug.Log("fromMaterial");
-            gameObject.GetComponent<MeshRenderer>().sharedMaterial = toMaterial;
-        }
-        else
-        {
-            Debug.Log("toMaterial");
-            gameObject.GetComponent<MeshRenderer>().sharedMaterial = fromMaterial;
-        }
-    }
+    [SerializeField] bool isRotating;
 
-    public void ObjRotate()
+    // 이번트 함수 목록
+    #region OnMouseDown
+
+    /// <summary>
+    /// 목표 회전값을 설정하는 함수
+    /// </summary>
+    public void SetTargetRotation()
     {
         targetRotation += 90;
     }
 
-    private void Update()
+    /// <summary>
+    /// 머터리얼을 전환시키는 함수
+    /// </summary>
+    public void ConvertMaterial()
     {
-        if (transform.rotation.eulerAngles.y < targetRotation)
+        if (gameObject.GetComponent<MeshRenderer>().sharedMaterial == fromMaterial)
         {
-            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+            gameObject.GetComponent<MeshRenderer>().sharedMaterial = toMaterial;
         }
         else
         {
+            gameObject.GetComponent<MeshRenderer>().sharedMaterial = fromMaterial;
+        }
+    }
+
+    #endregion
+    #region Update
+
+    /// <summary>
+    /// Y축의 회전각이 360도를 넘었을 경우,
+    /// <br></br>
+    /// Y축의 회전각을 0도로 초기화하는 함수
+    /// </summary>
+    private void ResetRotation()
+    {
+        if (Mathf.CeilToInt(transform.eulerAngles.y) >= 360)
+        {
+            targetRotation = 0;
+        }
+    }
+
+    /// <summary>
+    /// Transform을 회전시키는 함수
+    /// </summary>
+    private void RotateTransform()
+    {
+        if (transform.eulerAngles.y >= targetRotation)
+        {
+            isRotating = false;
+
             transform.eulerAngles = new Vector3(0, targetRotation, 0);
         }
+        else
+        {
+            isRotating = true;
+
+            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+        }
+    }
+
+    #endregion
+
+    private void OnMouseDown()
+    {
+        if (!isRotating) 
+        {
+            SetTargetRotation();
+            ConvertMaterial();
+        }
+    }
+
+    private void Update()
+    {
+        ResetRotation();
+        RotateTransform();
     }
 }
