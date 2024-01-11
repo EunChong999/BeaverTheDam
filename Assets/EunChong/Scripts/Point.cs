136e93edf2d18f504d5649d0cf5b4244d29db681
EunChong999 <eunchonggim999@gmail.com>
2024-01-11 14:53:39 +0900

    feat : 프레임 고정 기능 구현

--- Assets/EunChong/Scripts/Point.cs
+++ Assets/EunChong/Scripts/Point.cs
@@ -1,8 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static UnityEditor.Progress;
 
 public class Point : MonoBehaviour
 {
