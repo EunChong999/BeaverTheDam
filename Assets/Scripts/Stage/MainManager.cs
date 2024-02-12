7795c0de50ba36f1e87d09e4e5ef1ad7c13f3a06
jmjeon5533 <jmjeon5533@gmail.com>
2024-02-12 14:21:31 +0900

    feat : 스테이지 클리어 유무 표기 및 로컬 저장

--- Assets/Scripts/Stage/MainManager.cs
+++ Assets/Scripts/Stage/MainManager.cs
@@ -7,9 +7,14 @@ public class MainManager : Manager
 {
     [SerializeField] Transform endCard;
     [SerializeField] Transform[] star;
-    public int clearValue;
+    [SerializeField] GameObject nextStage;
+    public int clearScore;
+    public int StageIndex;
     private void Start() {
+        StageIndex = PlayerPrefs.GetInt("SelectIndex");
+        print(StageIndex);
         endCard.localScale = Vector3.zero;
+        nextStage.SetActive(StageIndex < PlayerPrefs.GetInt("MaxIndex"));
         for(int i = 0; i < star.Length; i++)
         {
             star[i].localScale = Vector3.zero;
@@ -19,19 +24,22 @@ public class MainManager : Manager
     {
         SceneMove(Scenes.SelectScene);
     }
-    public void ReStage(bool isRetry)
+    public void StartStage(bool isRetry)
     {
-        //if(!isRetry) stageIndex++;
+        if(!isRetry) PlayerPrefs.SetInt("SelectIndex",StageIndex + 1);
         SceneMove(Scenes.MainScene);
     }
     public void End()
     {
         StartCoroutine(EndMove());
+        var clearindex = StageIndex + 1;
+        if(clearindex > PlayerPrefs.GetInt("CanSelectIndex") && StageIndex < PlayerPrefs.GetInt("MaxIndex"))
+        PlayerPrefs.SetInt("CanSelectIndex", clearindex);
     }
     IEnumerator EndMove()
     {
         yield return endCard.DOScale(Vector3.one,0.5f).SetEase(Ease.OutBack).WaitForCompletion();
-        for(int i = 0; i < clearValue; i++)
+        for(int i = 0; i < clearScore; i++)
         {
             yield return star[i].DOScale(Vector3.one,0.5f).SetEase(Ease.OutBack).WaitForCompletion();
         }
--- Assets/Scripts/Stage/SelectManager.cs
+++ Assets/Scripts/Stage/SelectManager.cs
@@ -1,9 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SelectManager : Manager
 {
+    [SerializeField] Button[] Stagebutton;
+    public int clearIndex;
+    private void Start() {
+        //PlayerPrefs.SetInt("CanSelectIndex",0);
+        clearIndex = PlayerPrefs.GetInt("CanSelectIndex");
+        PlayerPrefs.SetInt("MaxIndex",Stagebutton.Length - 1);
+        print(clearIndex);
+        for(int i = 0; i < Stagebutton.Length; i++)
+        {
+            var num = i;
+            bool canSelect = num <= clearIndex;
+            Stagebutton[num].onClick.AddListener(() => 
+            {
+                if(canSelect)
+                {
+                    PlayerPrefs.SetInt("SelectIndex", num);
+                    StageStart();
+                }
+            });
+            Stagebutton[num].image.color = canSelect ? new Color(1,1,1,1) : new Color(0.5f,0.5f,0.5f,1);
+        }
+    }
     public void StageStart()
     {
         SceneMove(Scenes.MainScene);
