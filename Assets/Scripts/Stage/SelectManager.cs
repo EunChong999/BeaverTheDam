fbc9b7e0b02b6de0af897e4922032a73010deebc
jmjeon5533 <jmjeon5533@gmail.com>
2024-02-14 19:55:35 +0900

    feat : 챕터 구별 & 챕터 이동 애니메이션

--- Assets/Scripts/Stage/SelectManager.cs
+++ Assets/Scripts/Stage/SelectManager.cs
@@ -1,34 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class StageBtn
+{
+    public Button[] button;
+}
 public class SelectManager : Manager
 {
-    [SerializeField] Button[] Stagebutton;
+    [SerializeField] StageBtn[] buttons;
+    [SerializeField] Transform btnTransform;
+    [SerializeField] Button[] chapterBtn;
     public int clearIndex;
-    private void Start() {
-        //PlayerPrefs.SetInt("CanSelectIndex",0);
+    public int maxChapter;
+
+    int curChapter;
+    private void Start()
+    {
         clearIndex = PlayerPrefs.GetInt("CanSelectIndex");
-        PlayerPrefs.SetInt("MaxIndex",Stagebutton.Length - 1);
+        curChapter = PlayerPrefs.GetInt("curChapter");
+        maxChapter = buttons.Length - 1;
         print(clearIndex);
-        for(int i = 0; i < Stagebutton.Length; i++)
+        InitStageButton();
+        btnTransform.DOLocalMoveX(curChapter * -1920, 0);
+        ChapterMove(0);
+    }
+    public void ClearReset()
+    {
+        PlayerPrefs.SetInt("CanSelectIndex",0);
+        SceneMove(Scenes.SelectScene);
+    }
+    public void InitStageButton()
+    {
+        var maxIndex = 0;
+        for (int i = 0; i < buttons.Length; i++)
         {
-            var num = i;
-            bool canSelect = num <= clearIndex;
-            Stagebutton[num].onClick.AddListener(() => 
+            for (int j = 0; j < buttons[i].button.Length; j++)
             {
-                if(canSelect)
+                var numj = j;
+                var numi = i;
+                var max = maxIndex;
+                bool canSelect = numj + max <= clearIndex;
+                buttons[numi].button[numj].onClick.AddListener(() =>
                 {
-                    PlayerPrefs.SetInt("SelectIndex", num);
-                    StageStart();
-                }
-            });
-            Stagebutton[num].image.color = canSelect ? new Color(1,1,1,1) : new Color(0.5f,0.5f,0.5f,1);
+                    if (canSelect)
+                    {
+                        PlayerPrefs.SetInt("SelectIndex", numj + max);
+                        PlayerPrefs.SetInt("curChapter", curChapter);
+                        StageStart();
+                    }
+                });
+                buttons[numi].button[numj].image.color = canSelect ? new Color(1, 1, 1, 1) : new Color(0.5f, 0.5f, 0.5f, 1);
+            }
+            maxIndex += buttons[i].button.Length;
         }
+        PlayerPrefs.SetInt("MaxIndex", maxIndex);
     }
     public void StageStart()
     {
         SceneMove(Scenes.MainScene);
+    }
+    public void ChapterMove(int addIndex)
+    {
+        curChapter += addIndex;
+        chapterBtn[0].gameObject.SetActive(curChapter > 0);
+        chapterBtn[1].gameObject.SetActive(curChapter < maxChapter);
+        btnTransform.DOLocalMoveX(curChapter * -1920, 0.5f);
     }
 }
