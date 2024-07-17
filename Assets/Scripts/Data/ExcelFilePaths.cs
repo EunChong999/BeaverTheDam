b624860e202af89aa9ac85e256010953b2f6fef0
EunChong999 <eunchonggim999@gmail.com>
2024-07-17 22:37:05 +0900

    feat : 기능 개선

--- Assets/Scripts/Data/ExcelFilePaths.cs
+++ Assets/Scripts/Data/ExcelFilePaths.cs
@@ -3,6 +3,11 @@ using System.IO;
 
 public static class ExcelFilePaths
 {
-    public static readonly string DialogFilePath = "Assets/Scripts/Data/DialogDB.xlsx";
-    public static readonly string StageFilePath = "Assets/Scripts/Data/StageDB.xlsx";
+#if UNITY_EDITOR
+    public static readonly string DialogFilePath = Path.Combine(Application.dataPath, "Scripts", "Data", "DialogDB.xlsx");
+    public static readonly string StageFilePath = Path.Combine(Application.dataPath, "Scripts", "Data", "StageDB.xlsx");
+#else
+    public static readonly string DialogFilePath = Path.Combine(Application.persistentDataPath, "DialogDB.xlsx");
+    public static readonly string StageFilePath = Path.Combine(Application.persistentDataPath, "StageDB.xlsx");
+#endif
 }
